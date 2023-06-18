7a3708aeedb6dacfc1e2da78a41f7379f9b5cf98
akuizza <kalengbekas698@gmail.com>
2023-06-19 00:10:46 +0700

    tambah ai
    ai baru patrol
    pintu = rumah nomor 2 p1 p5

--- TALagi/Assets/Ija/Script3/AI.cs
+++ TALagi/Assets/Ija/Script3/AI.cs
@@ -1,20 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine;
+
+public enum MoveMode
+{
+    patrol, chase, wait
+}
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class AI : MonoBehaviour
 {
+    public MoveMode moveMode;
+
+    [Header("Steering")]
+    public float patrolSpeed;
+
+    [Header("Transform")]
+    public Transform[] patrolPoint;
+    public NavMeshAgent agent;
+
+    Vector3 destination;
+    int index_patrolpoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
+
+        if (agent.stoppingDistance < 0.5f)
+            agent.stoppingDistance = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (moveMode)
+        {
+            case MoveMode.patrol:
+                Patroling();
+                break;
+            case MoveMode.chase:
+                break;
+            case MoveMode.wait:
+                break;
+        }
+    }
+
+    void Patroling()
+    {
+        agent.speed = patrolSpeed;
+        agent.destination = destination = patrolPoint[index_patrolpoint].position;
+
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            index_patrolpoint = Random.Range(0, patrolPoint.Length);
+            Debug.Log("Ubah patrol ke " + index_patrolpoint.ToString());
+        }
     }
 }
