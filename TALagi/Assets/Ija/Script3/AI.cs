306d5d776aa96a45a66f42052a0e1ced75b30b8a
akuizza <kalengbekas698@gmail.com>
2023-06-19 23:44:24 +0700

    Update AI
    dah bisa ngejar tinggal tambahin buat playernya mati/pindah scene

--- TALagi/Assets/Ija/Script3/AI.cs
+++ TALagi/Assets/Ija/Script3/AI.cs
@@ -19,6 +19,11 @@ public class AI : MonoBehaviour
     public float maxTimeChasing;
     public float maxTimeWaiting;
     public float radiusHit;
+
+    [Header("Field Of View")]
+    public float viewRadius;
+    public float viewAngle;
+    public LayerMask obstacleMask;
     public LayerMask targetMask;
 
     [Header("Transform")]
@@ -29,6 +34,7 @@ public class AI : MonoBehaviour
     Vector3 destination;
     int index_patrolpoint;
     float currentTimeChasing, currentTimeWaiting;
+    bool isDetectTarget;
 
     // Start is called before the first frame update
     void Start()
@@ -55,15 +61,56 @@ public class AI : MonoBehaviour
                 Waiting();
                 break;
         }
+
+        FieldOfView();
+    }
+
+    void FieldOfView()
+    {
+        Collider[] range = Physics.OverlapSphere(transform.position, viewRadius, targetMask, QueryTriggerInteraction.Ignore);
+
+        if(range.Length > 0)
+        {
+            currentTarget = range[0].transform;
+
+            Vector3 direction = (currentTarget.position - transform.position).normalized;
+
+            if(Vector3.Angle(transform.forward, direction) < viewAngle / 2)
+            {
+                float m_distance = Vector3.Distance(transform.position, currentTarget.position);
+
+                if (!Physics.Raycast(transform.position, direction, m_distance, obstacleMask, QueryTriggerInteraction.Ignore))
+                {
+                    isDetectTarget = true;
+
+                    if ( moveMode != MoveMode.chase)
+                    {
+                        SwitchMoveMode(MoveMode.chase);
+                    }
+                }
+                else
+                {
+                    isDetectTarget = false;
+                }
+            }
+            else
+            {
+                isDetectTarget = false;
+            }
+        }
+        else
+        {
+            isDetectTarget = false;
+        }
     }
 
     void Waiting()
     {
-        agent.destination = transform.position;
+        
 
         if(currentTimeWaiting > maxTimeWaiting)
         {
-            moveMode = MoveMode.patrol;
+            SwitchMoveMode(MoveMode.patrol);
         }
         else
         {
@@ -74,12 +121,11 @@ public class AI : MonoBehaviour
     void Patroling()
     {
         agent.speed = patrolSpeed;
-        agent.destination = destination = patrolPoint[index_patrolpoint].position;
+        
 
         if (agent.remainingDistance < agent.stoppingDistance)
         {
-            index_patrolpoint = Random.Range(0, patrolPoint.Length);
-            Debug.Log("Ubah patrol ke " + index_patrolpoint.ToString());
+            SwitchMoveMode(MoveMode.wait);
         }
     }
 
@@ -100,12 +146,64 @@ public class AI : MonoBehaviour
 
         if(currentTimeChasing > maxTimeChasing)
         {
-            moveMode = MoveMode.patrol;
-            currentTimeChasing = 0;
+            SwitchMoveMode(MoveMode.wait);
         }
-        else
+        else if(!isDetectTarget)
         {
             currentTimeChasing += Time.deltaTime;
         }
+    }
+
+    void SwitchMoveMode(MoveMode m_moveMode)
+    {
+        switch (m_moveMode)
+        {
+            case MoveMode.patrol:
+
+                int lastindex = index_patrolpoint;
+                int newindex = Random.Range(0, patrolPoint.Length);
+
+                if(lastindex == newindex)
+                {
+                    newindex = Random.Range(0, patrolPoint.Length);
+                    Debug.Log("recalculate index");
+                    return;
+                }
+
+                index_patrolpoint = newindex;
+                agent.destination = destination = patrolPoint[index_patrolpoint].position;
+                Debug.Log("change patrol to " + index_patrolpoint.ToString());
+
+                break;
+            case MoveMode.chase:
+                currentTimeChasing = 0;
+                break;
+            case MoveMode.wait:
+                agent.destination = transform.position;
+                currentTimeWaiting = 0;
+                break;
+        }
+
+        moveMode = m_moveMode;
+        Debug.Log("change move mode to " + moveMode.ToString());
+    }
+
+    void OnDrawGizmos()
+    {
+        if (agent == null) return;
+
+        Gizmos.DrawWireSphere(transform.position, agent.stoppingDistance);
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
+
+        if (currentTarget != null && isDetectTarget)
+            Gizmos.DrawLine(transform.position, currentTarget.position);
+
+        float halfFov = viewAngle / 2f;
+        Quaternion leftRayRotation = Quaternion.AngleAxis(-halfFov, Vector3.up);
+        Quaternion rightRayRotation = Quaternion.AngleAxis(halfFov, Vector3.up);
+        Vector3 leftRayDirection = leftRayRotation * transform.forward;
+        Vector3 rightRayDirection = rightRayRotation * transform.forward;
+        Gizmos.DrawRay(transform.position, leftRayDirection * viewRadius);
+        Gizmos.DrawRay(transform.position, rightRayDirection * viewRadius);
     }
 }
