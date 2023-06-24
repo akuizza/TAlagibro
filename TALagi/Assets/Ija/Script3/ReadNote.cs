a167bdb255ebda498cca5647405254328107aed9
akuizza <kalengbekas698@gmail.com>
2023-06-24 13:11:51 +0700

    Note sistem
    tinggal poles sama nentuin tempatnya

---
+++ TALagi/Assets/Ija/Script3/ReadNote.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadNote : MonoBehaviour
+{
+     FPScript fpsPlayer;
+
+    public GameObject player;
+    public GameObject noteUI;
+    public GameObject pickUpText;
+
+    public bool inReach;
+    // Start is called before the first frame update
+    void Start()
+    {
+        noteUI.SetActive(false);
+        pickUpText.SetActive(false);
+
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            pickUpText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            pickUpText.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && inReach)
+        {
+            noteUI.SetActive(true);
+            player.GetComponent<FPScript>().enabled = false;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void ExitButton()
+    {
+        noteUI.SetActive(false);
+        player.GetComponent<FPScript>().enabled = true;
+    }
+}
