9ef13065b32ec8241c61c4c01567d7964a9d8162
akuizza <kalengbekas698@gmail.com>
2023-06-26 23:22:03 +0700

    update dikit

--- TALagi/Assets/Ija/Script3/Script Inventory/Inventory.cs
+++ TALagi/Assets/Ija/Script3/Script Inventory/Inventory.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<DataItem> items = new List<DataItem>();
+    public int maxSlotItem;
+
+    public void AddItem(DataItem new_item)
     {
-        
+        items.Add(new_item);
+        Debug.Log("item ditambahkan: " + new_item.item.m_name);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RemoveItem(DataItem removed_item)
     {
-        
+        for(int i = 0; i < items.Count; i++)
+        {
+            if (items[i].item.m_name == removed_item.item.m_name)
+            {
+                Debug.Log("item dihapus: " + items[i].item.m_name);
+                items.RemoveAt(i);
+            }
+        }
     }
 }
